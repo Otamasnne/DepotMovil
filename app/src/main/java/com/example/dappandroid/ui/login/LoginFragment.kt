cb4eab75ebf72c4709f39f6029745beb23605f3c
santi <santi.imas37@gmail.com>
2022-11-22 13:57:20 -0300

    ISSUE #1: intento de llamar al metodo login

--- app/src/main/java/com/example/dappandroid/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/dappandroid/ui/login/LoginFragment.kt
@@ -7,11 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
 import com.example.dappandroid.R
+import com.example.dappandroid.data.network.Resource
 
 
 class LoginFragment : Fragment() {
@@ -34,13 +39,24 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        viewModel.loginResponse.observe(viewLifecycleOwner, Observer {
+            when(it){
+                is Resource.Sucess -> {
+                    Toast.makeText(requireContext(), it.toString(), Toast.LENGTH_LONG).show()
+                }
+                is Resource.Failure -> {
+                    Toast.makeText(requireContext(), "Login Failure", Toast.LENGTH_LONG).show()
+                }
+            }
+        })
 
        val btnIngresar = view.findViewById<Button>(R.id.btnIngresar)
 
         btnIngresar.setOnClickListener{
 
-
+            val username = view.findViewById<EditText>(R.id.etUser).text.toString().trim()
+            val password = view.findViewById<EditText>(R.id.etPassword).text.toString().trim()
+            viewModel.login(username, password)
             findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
         }
 
--- app/src/main/java/com/example/dappandroid/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/dappandroid/ui/login/LoginViewModel.kt
@@ -3,18 +3,28 @@ package com.example.dappandroid.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.dappandroid.data.models.user.User
+import com.example.dappandroid.data.network.Resource
 import com.example.dappandroid.data.repositories.LoginRepository
 import com.example.dappandroid.util.Coroutines
+import kotlinx.coroutines.launch
 
 class LoginViewModel (
     private val repository: LoginRepository
         ) : ViewModel() {
 
-    private val _user = MutableLiveData<User>()
+    private val _loginResponse = MutableLiveData< Resource<User>>()
 
-    val user : LiveData<User>
-        get() = _user
+    val loginResponse : LiveData<Resource<User>>
+        get() = _loginResponse
+
+    fun login(
+        username: String,
+        password: String
+    ) = viewModelScope.launch{
+        _loginResponse.value = repository.login(username,password)
+    }
 
 //    fun login(username: String, password: String) {
 //        job = Coroutines.ioThenMain(
